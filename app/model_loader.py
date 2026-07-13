4c286eb76fa503fdc6babb0a6b9fa7155c911925
ananyamesta186@gmail.com <ngowdatv@gmail.com>
2026-07-13 12:25:00 +0530

    Load model from Hugging Face Hub

--- app/model_loader.py
+++ app/model_loader.py
@@ -1,16 +1,18 @@
 from pathlib import Path
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-# Get absolute path to the project root
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Absolute path to model folder
 MODEL_PATH = BASE_DIR / "model"
 
-print("Loading model from:", MODEL_PATH)
+HF_MODEL = "Ananya02/sentiment-analysis-distilbert"
 
-tokenizer = AutoTokenizer.from_pretrained(str(MODEL_PATH))
-
-model = AutoModelForSequenceClassification.from_pretrained(str(MODEL_PATH))
+if MODEL_PATH.exists():
+    print("Loading local model...")
+    tokenizer = AutoTokenizer.from_pretrained(str(MODEL_PATH))
+    model = AutoModelForSequenceClassification.from_pretrained(str(MODEL_PATH))
+else:
+    print("Loading model from Hugging Face Hub...")
+    tokenizer = AutoTokenizer.from_pretrained(HF_MODEL)
+    model = AutoModelForSequenceClassification.from_pretrained(HF_MODEL)
 
 print("Model loaded successfully!")
